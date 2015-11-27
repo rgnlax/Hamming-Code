ec2a43413c91416c8ee8d63db5578de271a974aa
rgnlax <rgnlax96@gmail.com>
2015-11-27 18:32:24 +0300

    Initial Commit

---
+++ Hamming Code/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hamming Code
+//
+//  Created by Maxim Pedchenko on 27.11.15.
+//  Copyright © 2015 MP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
